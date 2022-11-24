e778c510f5d01c68350420733a4b88556ab2fccb
Cristian <cristian-02-@live.com>
2022-11-23 21:27:40 -0500

    Diosito porfavor

--- shablagoo/settings.py
+++ shablagoo/settings.py
@@ -128,9 +128,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    
 
 
 # Default primary key field type
